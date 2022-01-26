29c6fd358719d76ff2a014893a6f5281ee54bbd1
tknovuran <tknovuran@TD61421>
2022-01-26 23:16:54 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abtract;
+using DataAccess.Abtract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+
+            //work code 
+            //are you  have a accsess ?
+          return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abtract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+
+                new Product { ProductId = 1,CategoryId = 1,ProductName ="Bardak",UnitPrice =15, UnitsInStock=15},
+                new Product { ProductId = 1,CategoryId = 1,ProductName ="kamera",UnitPrice =500, UnitsInStock=3},
+                new Product { ProductId = 2,CategoryId = 1,ProductName ="Kulaklık",UnitPrice =1500, UnitsInStock=2},
+                new Product { ProductId = 2,CategoryId = 1,ProductName ="Telefon",UnitPrice =150, UnitsInStock=65},
+                new Product { ProductId = 2,CategoryId = 1,ProductName ="Şarz Cihazı",UnitPrice =85, UnitsInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+          
+           
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün id'sine sahip listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;  
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            
+        }
+    }
+}
